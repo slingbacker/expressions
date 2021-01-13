e7dc82e7a97afcbc2e7f51f23702efda3071262a
slingbacker <77389458+slingbacker@users.noreply.github.com>
2021-01-13 14:32:06 +0000

    Update papercraft.js

--- papercraft.js
+++ papercraft.js
@@ -1,3 +1,5 @@
+// STOP MOTION 
+
 // Time Remap - 5 fps
 posterizeTime(5); 
 value 
@@ -9,9 +11,9 @@ wiggle(4,2.5)
 wiggle(0.9,4) 
 
 
-// Blinking Control
+// BLINKING CONTROL
 
-// EYES OPEN
+// EYES OPEN - opacity control
 
 transform.opacity
 
@@ -51,3 +53,59 @@ result = 0;
 
 }  
 result;
+
+
+// EYES CLOSED - opacity control (turns off whenever EYES OPEN is on)
+
+t = thisComp.layer("EYES OPEN").transform.opacity;  
+
+linear(t,0,100,100,0)  
+
+
+// SMILE
+
+minOnTime = 2.75;  
+
+maxOnTime = 4;  
+
+minOffTime = 2.5;  
+
+maxOffTime = 3;  
+
+i = 0;  
+
+tEnd = 0;  
+
+seedRandom(index,true);  
+
+preRun = random(maxOnTime);  
+
+while (time + preRun >= tEnd){  
+
+i++;  
+
+if(i%2){  
+
+tEnd += random(minOnTime,maxOnTime);  
+
+result = 100;  
+
+}else{  
+
+tEnd += random(minOffTime,maxOffTime);  
+
+result = 0;  
+
+}  
+
+}  
+
+result  
+
+
+// SIDE SMILE  opacity control (turns off whenever SMILE is on)
+
+t = thisComp.layer("Smile").transform.opacity;  
+
+linear(t,0,100,100,0)  
+
